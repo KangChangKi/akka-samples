a12e135d1f5c4c3e2f37afc2a2e3c2b138b5feb1
Kang Chang Ki <changki.kang@gmail.com>
2017-04-18 10:26:23 +0900

    Update to have 2 masters and 3 workers

--- akka-sample-cluster-scala/src/main/scala/sample/cluster/factorial/FactorialApp.scala
+++ akka-sample-cluster-scala/src/main/scala/sample/cluster/factorial/FactorialApp.scala
@@ -2,10 +2,11 @@ package sample.cluster.factorial
 
 object FactorialApp {
   def main(args: Array[String]): Unit = {
-    // starting 3 backend nodes and 1 frontend node
+    // starting 3 backend nodes and 2 frontend node
     FactorialBackend.main(Seq("2551").toArray)
     FactorialBackend.main(Seq("2552").toArray)
     FactorialBackend.main(Array.empty)
+    FactorialFrontend.main(Array.empty)
     FactorialFrontend.main(Array.empty)
   }
 }
